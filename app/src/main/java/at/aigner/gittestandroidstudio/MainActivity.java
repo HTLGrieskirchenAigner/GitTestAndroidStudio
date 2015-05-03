5563f10a85d7c311b410d546551ea89b59deb4f6
KlausAigner <kl.aigner@gmail.com>
2015-05-03 15:20:58 +0200

    comments added

--- app/src/main/java/at/aigner/gittestandroidstudio/MainActivity.java
+++ app/src/main/java/at/aigner/gittestandroidstudio/MainActivity.java
@@ -4,17 +4,21 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity
 {
+    TextView tv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // add some code here
+        tv = (TextView) findViewById(R.id.txt_hello);
+        tv.setText("Hallo Welt");
     }
 
 
