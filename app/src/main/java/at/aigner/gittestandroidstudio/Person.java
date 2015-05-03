756ade62d6ffd04fb756a71e16383edd89b2f8fd
BettinaAuinger <auinger_bettina@hotmail.com>
2015-05-03 16:29:53 +0200

    birthday added

--- app/src/main/java/at/aigner/gittestandroidstudio/Person.java
+++ app/src/main/java/at/aigner/gittestandroidstudio/Person.java
@@ -1,9 +1,12 @@
 package at.aigner.gittestandroidstudio;
 
+import java.util.Date;
+
 /**
  * Created by klaus on 03.05.2015.
  */
 public class Person {
     String firstname;
     String lastname;
+    Date birtday;
 }
